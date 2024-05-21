677038059e4d8a4f9d5f27011ba0fa580dfa1eae
siyajari samuel <siyajari.samuel@student.moringaschool.com>
2024-05-21 14:04:37 +0300

    Uptade changes

--- lib/app.py
+++ lib/app.py
@@ -1,16 +1,3 @@
-# lib/app.py
-print("Hello world!")
-print("hello sun")
-print("hello sky!")
-
-#lib/app.py
-
-print("Hello world!", end=" ")
-print("Hello world!", end=" ")
-print("Hello sun!", end="!! ")
-print("Hello sky!", end="!!! \n")
-
-
 
 
 print("Hello World! Pass this test, please.")
