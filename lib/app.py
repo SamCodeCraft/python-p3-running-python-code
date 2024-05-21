bea207922503c216617a31f50d6cdc06a34ec3fc
siyajari samuel <siyajari.samuel@student.moringaschool.com>
2024-05-21 13:52:16 +0300

    Run python code

---
+++ lib/app.py
@@ -0,0 +1,16 @@
+# lib/app.py
+print("Hello world!")
+print("hello sun")
+print("hello sky!")
+
+#lib/app.py
+
+print("Hello world!", end=" ")
+print("Hello world!", end=" ")
+print("Hello sun!", end="!! ")
+print("Hello sky!", end="!!! \n")
+
+
+
+
+print("Hello World! Pass this test, please.")
